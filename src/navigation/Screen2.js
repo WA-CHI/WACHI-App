5ec23149a8f84a4973252930cc690a2cea2d8231
홍서정 <94052799+hsj517@users.noreply.github.com>
2023-08-21 16:53:15 +0900

    feat : 파이어베이스 값 가져오기

--- src/navigation/Screen2.js
+++ src/navigation/Screen2.js
@@ -1,14 +1,33 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect  } from 'react';
+import { db } from '../utilities/firebase';
 import { format } from "date-fns";
 import { Calendar } from "react-native-calendars";
-import { View, Text,StyleSheet } from 'react-native';
+import { View, Text, Image, StyleSheet } from 'react-native';
+import firestore from '@react-native-firebase/firestore';
 
 //영상 조회 화면
 const Screen2 = () => {
   //날짜 선택
   const [selectedDate, setSelectedDate] = useState(null);
   const markedDates = selectedDate ? { [selectedDate]: { selected: true } } : {};
+  const [data, setData] = useState('');
+  //파이어베이스 값 가져오기
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const snapshot = await db.collection('imgsrc').doc('img').get();
+        if (snapshot.exists) {
+          const documentData = snapshot.data();
+          setData(documentData.src); // Replace 'yourField' with the actual field name in your document
+        }
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      }
+    };
 
+    fetchData();
+  }, []);
+  
   const handleDayPress = (date) => {
    // 현재 날짜와 비교하여 미래인 경우 선택하지 않음
    const currentDate = new Date();
@@ -21,9 +40,15 @@ const Screen2 = () => {
    setSelectedDate(date.dateString);
   };
 
+
+
   return (
     <View style={styles.container}>
-      <Text>Screen 2</Text>
+      <Text>{data}</Text>
+
+      <Image
+            source={require('C:/Users/hsj00/Desktop/WACHI/src/navigation/abc.jpg')}
+          />
       {selectedDate && <Text>조회: {selectedDate}</Text>}
       <Calendar  style={styles.calendar} onDayPress={handleDayPress}
         markedDates={markedDates}
